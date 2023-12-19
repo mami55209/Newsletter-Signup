8904c809bac01615162871c7b02a4993c9af3d05
Mahmoud Elsherbiny <mami55209@gmail.com>
2023-12-19 06:33:12 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+//initialize the app instance
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname +"/public/pages/signup.html");
+});
+app.post("/",(req,res)=>{
+    //our variables
+    const fName = req.body.fName;
+    const lName = req.body.lName;
+    const email = req.body.email;
+    const apiURL = "https://us21.api.mailchimp.com/3.0/lists/288714a94a";
+    const formData = {
+        members: [
+            {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+            FNAME: fName,
+            LNAME: lName
+                    }
+        }]
+    };
+    //converting the form object data into a string
+    const jsonFormData = JSON.stringify(formData);
+    const options = {
+        method:"POST",
+        auth: "4erben:c7434cbdb03727c792b4c097855d317e-us21",
+    };
+
+    const request = https.request(apiURL,options,(response)=>{
+   if(response.statusCode === 200){
+    res.sendFile(__dirname+"/public/pages/success.html");    
+   }else{
+    res.sendFile(__dirname+"/public/pages/failure.html");
+   }
+    })
+
+request.write(jsonFormData);
+request.end()
+});
+
+app.post("/hPage",(req,res)=>{
+    res.redirect("/");
+})
+app.listen(process.env.PORT || 3000, ()=>{
+});
+
+
+//c7434cbdb03727c792b4c097855d317e-us21 API-Key mailchimp
+//mailchimp unique id : 288714a94a
